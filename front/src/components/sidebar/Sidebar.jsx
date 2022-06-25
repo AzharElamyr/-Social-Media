4a88e05bd41406d8a70004cad5e79db6ac3b7340
Azhar Elamyr <azharelamyr@gmail.com>
2022-06-25 21:23:33 +0200

    home page which include topbar , sidebar ,feed , rightbar and topbar

---
+++ front/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      Sidebar
+      <div className="rightbarWrapper">wrapper content</div>
+    </div>
+  );
+}
